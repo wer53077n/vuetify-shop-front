8b9622656d0358bedf3740d2034bd1aea37951e2
LuChiaRong <wer53077n@gmail.com>
2024-07-23 10:46:08 +0800

    first commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,24 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+// Composables
+import { createVuetify } from "vuetify";
+import { zhHans, zhHant } from "vuetify/locale";
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    defaultTheme: "dark",
+  },
+  locale: {
+    locale: "zhHant",
+    messages: { zhHant },
+  },
+});
